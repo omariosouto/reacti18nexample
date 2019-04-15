77d9b11dee50abcd20165dde1d2a496571b0cd3e
Mario Souto <soutomarios@gmail.com>
2019-04-15 16:33:30 -0300

    update page links between then

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Home = (props) => {
-    console.log('Page:', props)
     return (
-        <h1>Bem vindo!</h1>
+        <div>
+            <h1>Bem vindo!</h1>
+            <Link to="/posts">Posts Page</Link>
+        </div>
     )
 }
--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Posts = (props) => {
     return (
-        <h1>Posts</h1>
+        <div>
+            <h1>Posts</h1>
+            <Link to="/">Home</Link>
+        </div>
     )
 }
